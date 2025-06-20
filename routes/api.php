2e5bbb31e246e279e87f085b7b3d65b9f3d65b01
Sebastián Diaz <sebastian.diaz@adviters.com>
2025-06-20 09:25:29 -0300

    add routes

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CardController;
+use App\Http\Controllers\PaymentController;
 
 Route::post('/cards', [CardController::class, 'store']);
+Route::post('/payments', [PaymentController::class, 'store']);
